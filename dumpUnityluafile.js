b256655973012b35626bdad5e17a737166410d73
灯、等灯等灯 <350030173@qq.com>
2022-12-15 13:08:07 +0800

    再重新排版

--- dumpUnityluafile.js
+++ dumpUnityluafile.js
@@ -1,5 +1,3 @@
-
-
 //
 //get_self_process_name()获取当前运行进程包名
 //参考：https://github.com/lasting-yang/frida_dump/blob/master/dump_dex_class.js
@@ -102,7 +100,7 @@ function modifyUnityLua()
 
         var moduleBaseAddress = Module.getBaseAddress("libil2cpp.so");
         var nativePointer = moduleBaseAddress.add(luaL_loadbuffer);
-        console.log("==nativePointer", nativePointer);
+        console.log("luaL_loadbuffer offset:" + nativePointer);
         Interceptor.attach(nativePointer,
             {
                 onEnter: function (args)
@@ -114,7 +112,7 @@ function modifyUnityLua()
                     if (name.indexOf("PlayerDataManager") >= 0)
                     {
                         //打印出想改的文件的字节码，打印出来后，复制到 icyberchef ,转换为16进制码，
-                        //再保存到010，再修改，注意字节码长度要一样，增加或删掉的字节码会导致文件长度不一样
+                        //再保存到010，再修改，注意字节码长度要一样（增加或删掉的字节码会导致文件长度不一样），否则可能出错
                         //可以利用里面的注释补齐字节码
                         /*console.log(hexdump(args[1].add(0x10),
                            {
@@ -124,8 +122,9 @@ function modifyUnityLua()
                                ansi: true
                            }
                        )); */
+
                         //需要保证写入的文件和原文件的长度必须一致，否则可能出错
-                        //也就是 参数中的size ，和写入的 mybuff 长度必须一致
+                        //也就是参数中的 size ，和写入的 mybuff 长度必须一致
                         Memory.writeByteArray(args[1].add(0x10), mybuff);
                     }
 
@@ -178,19 +177,20 @@ function dumpUnityLua()
 }
 
 /**
-
 unity中hook的函数
 // Namespace: LuaInterface
 public class LuaDLL // TypeDefIndex: 5749
 public static int luaL_loadbuffer(IntPtr luaState, byte[] buff, int size, string name); // 0x127E5DC
-
 */
 
 var soName = "libil2cpp.so"
 var luaL_loadbuffer = 0x127E5DC
+
 //重新写入的文件流
 var mybuff = [45, 45, 91, 91, 10, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61];
 var is32 = 0xc;//游戏32位写0xc, 游戏64位写0x14
 
 
 setImmediate(hook_dlopen);
+
+
